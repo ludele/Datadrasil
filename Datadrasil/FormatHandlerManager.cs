2d87b8baed762cc510167747ced8484323111c4f
ludele <Luddesnek@proton.me>
2023-10-10 10:24:41 +0200

    Added comments to the format handler manager

--- Datadrasil/FormatHandlerManager.cs
+++ Datadrasil/FormatHandlerManager.cs
@@ -10,16 +10,28 @@ namespace Datadrasil
 	{
 		private readonly Dictionary<string, IFormatHandler> formatHandlers;
 
+		/// <summary>
+		/// Initializes a new instance of the FormatHandlerManager class,
+		/// handling the redirection by creating links between format names and format handlers,
+		/// ensuring automatic selection based on the file's format.
+		/// </summary>
 		public FormatHandlerManager()
 		{
-			formatHandlers = new Dictionary<string, IFormatHandler>
+			formatHandlers = new Dictionary<string, IFormatHandler> 
 			{
-				{".json", new JSONFormatHandler() },
-				{".xml", new XMLFormatHandler() },
-				{".yaml", new YAMLFormatHandler() },
+				{ ".json", new JSONFormatHandler() },
+				{ ".xml", new XMLFormatHandler() },
+				{ ".yaml", new YAMLFormatHandler() },
 			};
 		}
 
+		/// <summary>
+		/// Reads data from a file based on its extension by constructing
+		/// the corresponding format handler for data reading.
+		/// </summary>
+		/// <param name="filePath">The path of the file to read.</param>
+		/// <returns>A list of objects representing the data read by the format handler.</returns>
+		/// <exception cref="NotSupportedException">Thrown if the file format is not supported.</exception>
 		public List<object> ReadData(string filePath)
 		{
 			string fileExtension = Path.GetExtension(filePath);
@@ -33,7 +45,13 @@ namespace Datadrasil
 				throw new NotSupportedException($"File format {fileExtension} is not supported.");
 			}
 		}
-
+		/// <summary>
+		/// Writes data to a file based on its extension by constructing
+		/// the corresponding format handler for data writing.
+		/// </summary>
+		/// <param name="filePath">The path of the file to write the data to.</param>
+		/// <param name="data">The list of objects representing the data to be written.</param>
+		/// <exception cref="NotSupportedException">Thrown if the file format is not supported.</exception>
 		public void WriteData(string filePath, List<object> data)
 		{
 			string fileExtension = Path.GetExtension(filePath);
